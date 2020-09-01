a2ee4917fd1f49366761dfc9bd215b4311b210c5
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-01 16:59:22 -0400

    Added for cycles to generate two arrays containing the respective values after simulation

--- proyecciones/proyecciones.rb
+++ proyecciones/proyecciones.rb
@@ -1,13 +1,3 @@
-def ventas_semestrales(arr, factor)
-    suma = 0
-    arr.each do |ele|
-        ele = ele*
-        suma += ele
-    end
-    return suma
-end
-
-
 #Lectura del archivo.
 data = open("ventas_base.db").read 
 
@@ -25,7 +15,39 @@ arr.map! do |ele|
     ele = ele.to_i
 end
 
-#Tercer test output
+#Tercer test output.
 print "#{arr}\n"
 
-#Usando el método ventas_semestrales
+#Generando los dos arreglos considerando las condiciones de cada simulación.
+primer_semestre = []
+segundo_semestre = []
+
+for i in 0..11 do
+    if i < 6
+        primer_semestre.push((arr[i]*1.10).truncate(2))
+    else
+        primer_semestre.push(arr[i].truncate(2))
+    end
+end
+
+for i in 0..11 do
+    if i > 5 
+        segundo_semestre.push((arr[i]*1.20).truncate(2))
+    else
+        segundo_semestre.push(arr[i].truncate(2))
+    end
+end
+
+#Cuarto test output
+puts "Probando arreglos simulados"
+puts "Arreglo original"
+print "#{arr}\n"
+puts "Simulación primer semestre"
+print "#{primer_semestre}\n"
+puts "Simulación segundo semestre"
+print "#{segundo_semestre}\n"
+
+
+
+
+
