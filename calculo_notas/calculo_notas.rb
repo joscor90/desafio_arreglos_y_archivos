5ff6d401f91734241bc37a89cc6a9a547b5deb31
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-01 17:51:39 -0400

    Added nota_mas_alta method and test output to calculo_notas.rb

--- calculo_notas/calculo_notas.rb
+++ calculo_notas/calculo_notas.rb
@@ -1,3 +1,10 @@
+def nota_mas_alta(arr)
+   arr.reject! {|ele| ele.class != Integer}
+   nota_mas_alta = arr.max()
+   return nota_mas_alta
+end
+
+
 #Abriendo y leyendo el archivo.
 require "csv"
 data = CSV.open("notas.data", converters: :numeric).readlines
@@ -5,5 +12,6 @@ data = CSV.open("notas.data", converters: :numeric).readlines
 #Primer output de prueba.
 print "#{data}\n"
 
-
-
+#Segundo output de prueba llamando al método.
+mas_alta = nota_mas_alta(data[1])
+puts mas_alta
