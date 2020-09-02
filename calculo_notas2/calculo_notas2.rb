68f2d32c7b63190dbfbba67e8f353facbf2cd2a9
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-02 09:03:40 -0400

    notas_mas_alta method was completed

--- calculo_notas2/calculo_notas2.rb
+++ calculo_notas2/calculo_notas2.rb
@@ -1,6 +1,13 @@
 #Método nota_mas_alta.
 def nota_mas_alta(arr)
+    notas_mas_altas = []
+    arr.each do |subarr|
+        int_notas = subarr.select {|ele| ele.class == Integer}
+        notas_mas_altas.push(int_notas.max())
+    end
+    return notas_mas_altas
 end
+
 #Requiriendo csv.
 require "csv"
 
@@ -9,3 +16,7 @@ data = CSV.open("notas.data", converters: :numeric).readlines
 
 #Primer output de prueba.
 print "#{data}\n"
+
+#Segundo output de prueba.
+puts "Arreglo con las notas más altas del curso"
+print "#{nota_mas_alta(data)}\n"
